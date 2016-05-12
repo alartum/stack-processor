9da1509fa8bce2abbd2f484d18f0267b9e8298c3
Aleksandr Artemenkov <alartum@gmail.com>
2016-05-12 03:01:59 +0300

    A lot of refactoring work done

--- stack-processor/main.c
+++ stack-processor/main.c
@@ -4,39 +4,57 @@
 #define PROJECT "StackProcessor"
 /// Version
 #define VERSION "2.1"
+#define DEBUG
 
 #include <stdio.h>
 #include "mylib.h"
 #include <string.h>
 #include <limits.h>
+#include "stack.h"
 #include "CPU.h"
+
+
 
 int main (int argc, char* argv[])
 {
     CHECK_DEFAULT_ARGS();
-    char programName[NAME_MAX] = {};
+    char prog_name[NAME_MAX] = {};
     switch (argc)
     {
     case 2:
-        strcpy (programName, argv[1]);
+        strcpy (prog_name, argv[1]);
         break;
     default:
         WRITE_WRONG_USE();
     }
+    // Testing staff
+    /*
+    stack_t stack;
+    stack_t_construct(&stack);
+    char a = 'a', b = 'b';
+    stack_t_push(&stack, &a, 1);
+    stack_t_dump(&stack);
+    stack_t_push(&stack, &b, 1);
+    stack_t_dump(&stack);
+    stack_t_pop(&stack, &a, 1);
+    stack_t_dump(&stack);
+    printf ("a = %c, b = %c\n", a, b);
+    stack_t_destruct(&stack);
+    */
 
     Buffer program;
-    buffer_construct (&program, programName);
-    CPU cpu = {};
-    cpu_construct(&cpu);
-    cpu_set_program(&cpu, &program);
-    if (!cpu_execute(&cpu))
+    buffer_construct (&program, prog_name);
+    cpu_t cpu;
+    cpu_t_construct(&cpu);
+    cpu_t_set_program(&cpu, &program);
+    if (!cpu_t_execute(&cpu))
     {
-        cpu_destruct(&cpu);
+        cpu_t_destruct(&cpu);
         COMMENT ("Runtime error occured!");
 
         return WRONG_RESULT;
     }
-    cpu_destruct (&cpu);
+    cpu_t_destruct (&cpu);
     buffer_destruct (&program);
 
     return NO_ERROR;
